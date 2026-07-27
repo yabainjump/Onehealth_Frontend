2672c7021a28d6f3701ba44722f145e1fee199d8
Hussein Menkam <93983437+yabainjump@users.noreply.github.com>
2026-07-27 06:38:50 +0100

    map update

--- src/app/core/constants/map.constants.ts
+++ src/app/core/constants/map.constants.ts
@@ -3,6 +3,7 @@
  * Elles restent chargées directement comme images par le navigateur.
  */
 export const OPENSTREETMAP_TILE_URL =
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?ngsw-bypass=true';
+  'https://tile.openstreetmap.org/{z}/{x}/{y}.png?ngsw-bypass=true';
 
-export const OPENSTREETMAP_ATTRIBUTION = '© OpenStreetMap';
+export const OPENSTREETMAP_ATTRIBUTION =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
